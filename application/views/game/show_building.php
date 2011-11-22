33019902dcc8fbeecf103ed557668b633e7c1532
Dexyne <enyriel@gmail.com>
2011-11-22 17:30:47 +0100

    Modification de la vue listant les batiments

--- application/views/game/show_building.php
+++ application/views/game/show_building.php
@@ -9,13 +9,18 @@
 	<tr>
 		<th>&nbsp;</th>
 		<th>Informations</th>
+		<th>Level</th>
 		<th>Actions</th>
 	</tr>
 <?php foreach($building_list as $building) : ?>
 	<tr>
 		<td><?php echo img("building/{$building->name_clean}.gif", "Illustration : {$building->name}", $building->name) ?></td>
 		<td>
-			<h4><?php echo $building->name ?></h4>
+			<h4>
+				<?php echo $building->name ?>
+			<?php if($building->important > 0)
+				echo "<span class=\"label important\">Important</span>"; ?>
+			</h4>
 			<p><?php echo $building->content ?></p>
 			<dl>
 				<dt>Ressource nécessaire :<dt>
@@ -27,6 +32,7 @@
 				</dd>
 			</dl>
 		</td>
+		<td class="center">0</td>
 		<td><?php echo anchor("game/building/create/{$building->id}", "Construire ?"); ?></td>
 	</tr>
 <?php endforeach; ?>		
