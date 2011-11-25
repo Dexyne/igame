aed1c184b62db3979fd9b01ab2420024635c2624
Dexyne <enyriel@gmail.com>
2011-11-25 20:30:25 +0100

    planets_model devient planet_model

---
+++ application/models/planet_model.php
@@ -0,0 +1,58 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Planet_model extends CI_Model {
+
+	protected $_table = 'planets';
+
+	public function get_allResources()
+	{
+		return $this->db->select('metal, crystal, deuterium, energy_used, energy_max')
+			->from($this->_table)
+			->get()
+			->result();
+	}
+
+	public function get_allResourcesByPlanet($planet_id)
+	{
+		return $this->db->select('metal, crystal, deuterium, energy_used, energy_max')
+			->from($this->_table)
+			->where('id', $planet_id)
+			->get()
+			->result();
+	}
+
+	public function get_allPlanetByuser($user_id)
+	{
+		return $this->db->select('id, name')
+			->from($this->_table)
+			->where('user_id', $user_id)
+			->get()
+			->result();
+	}
+
+	public function get_planet($planet_id)
+	{
+		return $this->db->select('*')
+			->from($this->_table)
+			->where('id', $planet_id)
+			->get()
+			->result();
+	}
+
+	public function save($planet_id, $data)
+	{
+		return $this->db->set('user_id', $data['user_id'])
+			->set('name', $data['name'])
+			->set('galaxy', $data['galaxy'])
+			->set('system', $data['system'])
+			->set('planet', $data['planet'])
+			->set('last_update', $data['last_update'], false)
+			->set('metal', $data['metal'])
+			->set('crystal', $data['crystal'])
+			->set('deuterium', $data['deuterium'])
+			->set('energy_used', $data['energy_used'])
+			->set('energy_max', $data['energy_max'])
+			->where('id', $planet_id)
+			->insert($this->_table);
+	}
+}
