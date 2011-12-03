64a66fac8bfae03ddc55ca7bf9dfce0ac7549792
Dexyne <enyriel@gmail.com>
2011-12-03 16:38:29 +0100

    Modification de l'affichage de la liste des batiments

--- application/models/planet_model.php
+++ application/models/planet_model.php
@@ -12,6 +12,7 @@ class Planet_model extends CI_Model {
 			->result();
 	}
 
+	// Retourne toutes les ressources d'une planète
 	public function get_allResourcesByPlanet($planet_id)
 	{
 		return $this->db->select('metal, crystal, deuterium, energy_used, energy_max')
@@ -21,6 +22,7 @@ class Planet_model extends CI_Model {
 			->result();
 	}
 
+	// Retourne l'id et le nom des planètes d'un utilisateur
 	public function get_allPlanetByuser($user_id)
 	{
 		return $this->db->select('id, name')
@@ -30,16 +32,18 @@ class Planet_model extends CI_Model {
 			->result();
 	}
 
-	public function get_planet($planet_id)
+	// Retourne les données de la planète demandé
+	public function get_planet($planet_id, $select = '*')
 	{
-		return $this->db->select('*')
+		return $this->db->select($select)
 			->from($this->_table)
 			->where('id', $planet_id)
 			->get()
 			->result();
 	}
 
-	public function save($planet_id, $data)
+	// Créer une nouvelle planète dans la DB
+	public function create($planet_id, $data)
 	{
 		return $this->db->set('user_id', $data['user_id'])
 			->set('name', $data['name'])
@@ -54,5 +58,23 @@ class Planet_model extends CI_Model {
 			->set('energy_max', $data['energy_max'])
 			->where('id', $planet_id)
 			->insert($this->_table);
+	}
+
+	// Mettre à jour la table planets
+	public function update($planet_id, $data)
+	{
+		return $this->db->set('user_id', $data['user_id'])
+			->set('name', $data['name'])
+			->set('galaxy', $data['galaxy'])
+			->set('system', $data['system'])
+			->set('planet', $data['planet'])
+			->set('last_update', $data['last_update'], false)
+			->set('metal', $data['metal'])
+			->set('crystal', $data['crystal'])
+			->set('deuterium', $data['deuterium'])
+			->set('energy_used', $data['energy_used'])
+			->set('energy_max', $data['energy_max'])
+			->where('id', $planet_id)
+			->update($this->_table);
 	}
 }
