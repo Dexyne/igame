4f0081ae44ab5ba5b75e8d7fde876e263ae4b052
Dexyne <enyriel@gmail.com>
2011-12-10 20:07:08 +0100

    DB update

--- igame.sql
+++ igame.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Dim 04 Décembre 2011 à 18:52
+-- Généré le : Sam 10 Décembre 2011 à 17:15
 -- Version du serveur: 5.1.49
 -- Version de PHP: 5.3.3-7+squeeze3
 
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `building` (
   `energy` int(10) DEFAULT NULL,
   `multiplier` float NOT NULL DEFAULT '1.1',
   `square` int(5) NOT NULL,
-  `construct_time` datetime NOT NULL,
+  `construct_time` int(11) NOT NULL,
   `previous_construct` varchar(255) NOT NULL,
   `important` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
@@ -47,10 +47,10 @@ CREATE TABLE IF NOT EXISTS `building` (
 --
 
 INSERT INTO `building` (`id`, `name`, `name_clean`, `content`, `metal`, `crystal`, `deuterium`, `energy`, `multiplier`, `square`, `construct_time`, `previous_construct`, `important`) VALUES
-(1, 'Mine de métal', 'metal_mine', 'Une mine permettant d''extraire les métaux.', 50, 50, NULL, 20, 1.1, 2, '0000-00-00 00:02:00', '0', 1),
-(2, 'Mine de cristal', 'crystal_mine', 'Une mine permettant d''extraire les cristaux.', 50, 50, NULL, 20, 1.1, 2, '0000-00-00 00:02:00', '0', 1),
-(3, 'Synthétiseur de deuterium', 'deuterieum_synthetiser', 'Extrait et synthétise le deutérium.', 60, 30, NULL, 25, 1.1, 3, '0000-00-00 00:03:00', '0', 1),
-(4, 'Centrale solaire', 'solar_plant', 'Récupère de l''énergie grâce au soleil, source inépuisable.', 50, 40, NULL, NULL, 1.1, 3, '0000-00-00 00:02:00', '0', 1);
+(1, 'Mine de métal', 'metal_mine', 'Une mine permettant d''extraire les métaux.', 50, 50, NULL, 20, 1.1, 2, 120, '0', 1),
+(2, 'Mine de cristal', 'crystal_mine', 'Une mine permettant d''extraire les cristaux.', 50, 50, NULL, 20, 1.1, 2, 120, '0', 1),
+(3, 'Synthétiseur de deuterium', 'deuterieum_synthetiser', 'Extrait et synthétise le deutérium.', 60, 30, NULL, 25, 1.1, 3, 120, '0', 1),
+(4, 'Centrale solaire', 'solar_plant', 'Récupère de l''énergie grâce au soleil, source inépuisable.', 50, 40, NULL, NULL, 1.1, 3, 120, '0', 1);
 
 -- --------------------------------------------------------
 
@@ -73,8 +73,7 @@ CREATE TABLE IF NOT EXISTS `igame_sessions` (
 --
 
 INSERT INTO `igame_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-('934ccf3603ab9e215080273dd7680e9a', '192.168.233.1', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2', 1323017309, 'a:6:{s:9:"user_data";s:0:"";s:8:"username";s:4:"test";s:5:"email";s:12:"test@mail.fr";s:2:"id";i:1;s:6:"logged";b:1;s:9:"planet_id";s:1:"1";}'),
-('91594b70f9c40ba1f322e089b8fba4bf', '192.168.233.1', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2', 1323020996, 'a:6:{s:9:"user_data";s:0:"";s:8:"username";s:4:"test";s:5:"email";s:12:"test@mail.fr";s:2:"id";i:1;s:6:"logged";b:1;s:9:"planet_id";s:1:"1";}');
+('5aa2309d5ef581c38f1f0393e58cafe3', '192.168.233.1', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2', 1323466175, 'a:6:{s:9:"user_data";s:0:"";s:8:"username";s:4:"test";s:5:"email";s:12:"test@mail.fr";s:2:"id";i:1;s:6:"logged";b:1;s:9:"planet_id";s:1:"1";}');
 
 -- --------------------------------------------------------
 
@@ -108,7 +107,7 @@ CREATE TABLE IF NOT EXISTS `planets` (
 --
 
 INSERT INTO `planets` (`id`, `user_id`, `name`, `galaxy`, `system`, `created_at`, `updated_at`, `planet`, `metal`, `crystal`, `deuterium`, `energy_used`, `energy_max`, `metal_mine`, `crystal_mine`, `deuterium_sintetizer`, `solar_plant`) VALUES
-(1, 1, 'planète de test', 1, 1, '2011-12-01 00:00:00', '2011-12-04 18:50:00', 1, 8470.46508907, 9404.82777778, 8969.22777778, 20, 100, 1, 0, 0, 3),
+(1, 1, 'planète de test', 1, 1, '2011-12-01 00:00:00', '2011-12-09 22:31:19', 1, 20839.29842240, 21773.66111111, 21338.06111111, 20, 100, 1, 0, 0, 3),
 (2, 1, 'youpi', 1, 1, '2011-12-02 00:00:00', '2011-12-04 18:49:58', 3, 14015.55555555, 16990.65555555, 13466.45555555, 50, 50, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
@@ -120,27 +119,19 @@ INSERT INTO `planets` (`id`, `user_id`, `name`, `galaxy`, `system`, `created_at`
 CREATE TABLE IF NOT EXISTS `queue` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `element_id` int(11) NOT NULL,
+  `element_type` varchar(50) DEFAULT NULL,
   `planet_id` int(11) NOT NULL,
   `time_start` datetime NOT NULL,
   `time_finish` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Contenu de la table `queue`
 --
 
-INSERT INTO `queue` (`id`, `element_id`, `planet_id`, `time_start`, `time_finish`) VALUES
-(1, 1, 1, '2011-12-04 17:52:43', '0000-00-00 00:00:00'),
-(2, 1, 1, '2011-12-04 17:53:15', '0000-00-00 00:00:00'),
-(3, 1, 1, '2011-12-04 18:06:21', '2011-12-04 18:06:21'),
-(4, 1, 1, '2011-12-04 18:07:10', '2011-12-04 18:07:10'),
-(5, 1, 1, '2011-12-04 18:07:11', '2011-12-04 18:07:11'),
-(6, 1, 1, '2011-12-04 18:21:12', '2011-12-04 18:21:12'),
-(7, 1, 1, '2011-12-04 18:21:45', '2011-12-04 18:21:45'),
-(8, 1, 1, '2011-12-04 18:24:52', '2011-12-04 18:24:52'),
-(9, 1, 1, '2011-12-04 18:40:23', '2011-12-04 18:40:23'),
-(10, 1, 1, '2011-12-04 18:40:31', '2011-12-04 18:40:31');
+INSERT INTO `queue` (`id`, `element_id`, `element_type`, `planet_id`, `time_start`, `time_finish`) VALUES
+(1, 1, 'building', 1, '2011-12-09 22:33:08', '2011-12-09 22:35:08');
 
 -- --------------------------------------------------------
 
