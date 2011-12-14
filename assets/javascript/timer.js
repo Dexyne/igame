eb549f56147e6be72da1ada56a41607d54fe6081
Dexyne <enyriel@gmail.com>
2011-12-14 20:08:01 +0100

    Add content of construct timer

---
+++ assets/javascript/timer.js
@@ -0,0 +1,29 @@
+$(function(){
+
+	callback = function(){
+		$(".construction").each(function(){
+			
+			$construction = $(this);
+			$remainingTime = $construction.find(".remaining-time");
+
+			now = new Date();
+			date = new Date($construction.data("finish-at"));
+			difference = date - now;
+
+			if(difference <= 0) {
+				id = $construction.data("id");
+
+				$.ajax({
+					url: 		'queue/update/' + id,
+					success: 	function(){
+						document.location.href = "http://192.168.233.129:2001/index.php/game/building.html";
+					}
+				});
+			} else {
+				$remainingTime.text(difference + " sec restantes…");
+			}			
+		});
+	}
+
+	setInterval(callback, 1000);
+});
