50f2156c7a2f76b9a27a977ddffada7d9b3a6dea
Dexyne <enyriel@gmail.com>
2011-12-19 17:46:32 +0100

    Update DB

--- igame.sql
+++ igame.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Sam 10 Décembre 2011 à 17:15
+-- Généré le : Lun 19 Décembre 2011 à 17:45
 -- Version du serveur: 5.1.49
 -- Version de PHP: 5.3.3-7+squeeze3
 
@@ -23,6 +23,8 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
 -- Structure de la table `building`
+--
+-- Création: Jeu 08 Décembre 2011 à 19:19
 --
 
 CREATE TABLE IF NOT EXISTS `building` (
@@ -57,6 +59,9 @@ INSERT INTO `building` (`id`, `name`, `name_clean`, `content`, `metal`, `crystal
 --
 -- Structure de la table `igame_sessions`
 --
+-- Création: Sam 03 Décembre 2011 à 16:20
+-- Dernière modification: Lun 19 Décembre 2011 à 17:26
+--
 
 CREATE TABLE IF NOT EXISTS `igame_sessions` (
   `session_id` varchar(40) NOT NULL DEFAULT '0',
@@ -73,12 +78,14 @@ CREATE TABLE IF NOT EXISTS `igame_sessions` (
 --
 
 INSERT INTO `igame_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-('5aa2309d5ef581c38f1f0393e58cafe3', '192.168.233.1', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2', 1323466175, 'a:6:{s:9:"user_data";s:0:"";s:8:"username";s:4:"test";s:5:"email";s:12:"test@mail.fr";s:2:"id";i:1;s:6:"logged";b:1;s:9:"planet_id";s:1:"1";}');
+('9bf44cb0869bb4c3e65b0f576d4b68b4', '192.168.233.1', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7', 1324311892, 'a:6:{s:9:"user_data";s:0:"";s:8:"username";s:4:"test";s:5:"email";s:12:"test@mail.fr";s:2:"id";i:1;s:6:"logged";b:1;s:9:"planet_id";s:1:"1";}');
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `planets`
+--
+-- Création: Sam 17 Décembre 2011 à 20:22
 --
 
 CREATE TABLE IF NOT EXISTS `planets` (
@@ -87,9 +94,9 @@ CREATE TABLE IF NOT EXISTS `planets` (
   `name` varchar(255) NOT NULL,
   `galaxy` tinyint(2) NOT NULL,
   `system` tinyint(2) NOT NULL,
+  `planet` tinyint(2) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `planet` tinyint(2) NOT NULL,
   `metal` double(132,8) NOT NULL DEFAULT '0.00000000',
   `crystal` double(132,8) NOT NULL DEFAULT '0.00000000',
   `deuterium` double(132,8) NOT NULL DEFAULT '0.00000000',
@@ -106,14 +113,16 @@ CREATE TABLE IF NOT EXISTS `planets` (
 -- Contenu de la table `planets`
 --
 
-INSERT INTO `planets` (`id`, `user_id`, `name`, `galaxy`, `system`, `created_at`, `updated_at`, `planet`, `metal`, `crystal`, `deuterium`, `energy_used`, `energy_max`, `metal_mine`, `crystal_mine`, `deuterium_sintetizer`, `solar_plant`) VALUES
-(1, 1, 'planète de test', 1, 1, '2011-12-01 00:00:00', '2011-12-09 22:31:19', 1, 20839.29842240, 21773.66111111, 21338.06111111, 20, 100, 1, 0, 0, 3),
-(2, 1, 'youpi', 1, 1, '2011-12-02 00:00:00', '2011-12-04 18:49:58', 3, 14015.55555555, 16990.65555555, 13466.45555555, 50, 50, 0, 0, 0, 0);
+INSERT INTO `planets` (`id`, `user_id`, `name`, `galaxy`, `system`, `planet`, `created_at`, `updated_at`, `metal`, `crystal`, `deuterium`, `energy_used`, `energy_max`, `metal_mine`, `crystal_mine`, `deuterium_sintetizer`, `solar_plant`) VALUES
+(1, 1, 'planète de test', 1, 1, 1, '2011-12-01 00:00:00', '2011-12-19 17:27:47', 43983.90953352, 44918.27222223, 44482.67222223, 20, 100, 2, 2, 0, 3),
+(2, 1, 'youpi', 1, 1, 3, '2011-12-02 00:00:00', '2011-12-10 18:12:14', 28352.66666666, 31327.76666666, 27803.56666666, 50, 50, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `queue`
+--
+-- Création: Jeu 08 Décembre 2011 à 19:38
 --
 
 CREATE TABLE IF NOT EXISTS `queue` (
@@ -130,13 +139,13 @@ CREATE TABLE IF NOT EXISTS `queue` (
 -- Contenu de la table `queue`
 --
 
-INSERT INTO `queue` (`id`, `element_id`, `element_type`, `planet_id`, `time_start`, `time_finish`) VALUES
-(1, 1, 'building', 1, '2011-12-09 22:33:08', '2011-12-09 22:35:08');
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `ship`
+--
+-- Création: Sam 03 Décembre 2011 à 16:20
 --
 
 CREATE TABLE IF NOT EXISTS `ship` (
@@ -154,6 +163,8 @@ CREATE TABLE IF NOT EXISTS `ship` (
 --
 -- Structure de la table `technology`
 --
+-- Création: Sam 03 Décembre 2011 à 16:20
+--
 
 CREATE TABLE IF NOT EXISTS `technology` (
   `id` int(11) NOT NULL,
@@ -169,6 +180,8 @@ CREATE TABLE IF NOT EXISTS `technology` (
 
 --
 -- Structure de la table `users`
+--
+-- Création: Sam 03 Décembre 2011 à 16:20
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
