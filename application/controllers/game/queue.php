c3a67a401be422404b1566e7e566722d9cd5867e
Dexyne <enyriel@gmail.com>
2011-12-20 17:19:07 +0100

    Add header for GNU/GPL license

--- application/controllers/game/queue.php
+++ application/controllers/game/queue.php
@@ -1,5 +1,30 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ * This file is part of iGame
+ *
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @see https://github.com/Dexyne/igame
+ *
+ * @copyright Copyright (c) 2011-Present, Aurélien Léger, iGame
+ * All rights reserved.
+ *
+ * iGame is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iGame program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this iGame.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * #######################################################################
+ */
+
 class Queue extends CI_Controller {
 
 	public function __construct()
