2f886dc72b245d74a6d8549ac8a5a0d854c91a0e
Dexyne <enyriel@gmail.com>
2011-12-23 12:41:22 +0100

    Update for the GPL license

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,12 +1,42 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ * This file is part of iGame
+ *
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @see https://github.com/Dexyne/igame
+ *
+ * @copyright Copyright (c) 2011-Present, Aurélien Léger, iGame
+ * All rights reserved.
+ *
+ * iGame is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iGame program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with iGame.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * #######################################################################
+ */
+
 class User_model extends CI_Model {
 
 	protected $_table = 'users';
 
 	public function save($data) 
 	{
-		return $this->db->insert($this->_table, $data);
+		return $this->db->set('username', $data['username'])
+			->set('email', $data['email'])
+			->set('password', $data['password'])
+			->set('created_at', $data['created_at'], false)
+			->set('updated_at', $data['updated_at'], false)
+			->insert($this->_table);
 	}
 
 	public function check_id($email, $password)
@@ -33,9 +63,23 @@ class User_model extends CI_Model {
 			->result();
 	}
 
+	public function get_userById($id, $select = '*')
+	{
+		return $this->db->select($select)
+			->from($this->_table)
+			->where('id', $id)
+			->get()
+			->result();
+	}
+
 	public function getId()
 	{
 		$this->db->where('username', $this->session->userdata('username'));
 		return $this->db->get('users');
+	}
+
+	public function last_id()
+	{
+		return $this->db->insert_id();
 	}
 }
