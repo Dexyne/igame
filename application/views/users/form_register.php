71a49899cf85215a8093d8c3ae32c521a6e640f7
Dexyne <enyriel@gmail.com>
2011-12-23 12:22:03 +0100

    Update to avoid conflict with connection

--- application/views/users/form_register.php
+++ application/views/users/form_register.php
@@ -6,20 +6,20 @@
 <?php if(isset($notif)) : echo notif($notif['type'], $notif['message']); else : echo ''; endif; ?>
 
 <?php echo form_open('users/register'); ?>
-	<label for="user[name]">Pseudo :</label>
-	<input type="text" id="user[name]" name="user[name]" value="<?php echo set_value('user[name]') ?>">
+	<label for="new_user[name]">Pseudo :</label>
+	<input type="text" id="new_user[name]" name="new_user[name]" value="<?php echo set_value('new_user[name]') ?>">
 	<div class="clear"></div>
 
-	<label for="user[email]">E-mail :</label>
-	<input type="text" id="user[email]" name="user[email]" value="<?php echo set_value('user[email]') ?>">
+	<label for="new_user[email]">E-mail :</label>
+	<input type="text" id="new_user[email]" name="new_user[email]" value="<?php echo set_value('new_user[email]') ?>">
 	<div class="clear"></div>
 
-	<label for="user[password]">Mot de passe :</label>
-	<input type="password" id="user[password]" name="user[password]" value="">
+	<label for="new_user[password]">Mot de passe :</label>
+	<input type="password" id="new_user[password]" name="new_user[password]" value="">
 	<div class="clear"></div>
 
-	<label for="user[pass_conf]">Confirmation du mot de passe :</label>
-	<input type="password" id="user[pass_conf]" name="user[pass_conf]" value="">
+	<label for="new_user[pass_conf]">Confirmation du mot de passe :</label>
+	<input type="password" id="new_user[pass_conf]" name="new_user[pass_conf]" value="">
 	<div class="clear"></div>
 
 	<input type="submit" value="S'inscrire" class="btn primary">
