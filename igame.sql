e9918616490272e5765283be160f0ef7fa014844
Dexyne <enyriel@gmail.com>
2012-02-04 21:24:17 +0100

    Update SQL Database : Add dependencies

--- igame.sql
+++ igame.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Lun 26 Décembre 2011 à 22:40
+-- Généré le : Sam 04 Février 2012 à 21:18
 -- Version du serveur: 5.1.49
 -- Version de PHP: 5.3.3-7+squeeze3
 
@@ -38,21 +38,21 @@ CREATE TABLE IF NOT EXISTS `building` (
   `multiplier` float NOT NULL DEFAULT '1.1',
   `square` int(5) NOT NULL,
   `construct_time` int(11) DEFAULT NULL,
-  `previous_construct` varchar(255) NOT NULL,
   `important` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Contenu de la table `building`
 --
 
-INSERT INTO `building` (`id`, `name`, `name_clean`, `introduction`, `content`, `metal`, `crystal`, `deuterium`, `energy`, `multiplier`, `square`, `construct_time`, `previous_construct`, `important`) VALUES
-(1, 'Mine de métal', 'metal_mine', 'Une mine permettant d''extraire les métaux.', 'Principale matière première pour la construction de bâtiments et de vaisseaux. Le métal est la matière première la moins chère, mais c''est la plus importante. La production du métal consomme moins d''énergie que la production des autres ressources. Plus les mines sont développées, plus elles sont profondes. Les réserves minérales sont situées en profondeur sur la majorité des planètes. Les mines les plus profondes permettent d''extraire plus de métal, ce qui augment leur rendement. Cependant, les mines les plus développées consomment aussi plus d''énergie.', 50, 50, NULL, 8, 1.4, 2, 120, '0', 1),
-(2, 'Mine de cristal', 'crystal_mine', 'Une mine permettant d''extraire les cristaux.', 'Le cristal est la principale ressource pour l''électronique et pour les alliages et son extraction consomme environ une fois et demi plus d''énergie que celle du métal, le cristal est donc plus précieux. Tous les vaisseaux et bâtiments ont besoin de cristal. Malheureusement, la plupart des cristaux nécessaires pour la construction de vaisseaux sont très rares et se trouvent en grande profondeur, comme le métal. La production augmente avec le développement des mines car on atteint des gisement plus grands et plus purs.', 50, 50, NULL, 8, 1.4, 2, 120, '0', 1),
-(3, 'Synthétiseur de deuterium', 'deuterium_synthesizer', 'Extrait et synthétise le deutérium.', 'Le deutérium est produit à partir d''eau lourde. Les plus grandes quantités se trouvent donc en grande profondeur, comme le métal et le cristal. Le développement du bâtiment permet d''accéder à des réserves de deutérium en plus grande profondeur et de concentrer ce deutérium extrait de l''eau lourde. Le deutérium sert de carburant pour les vaisseaux et est nécessaire pour presque toutes les recherches.', 60, 30, NULL, 12, 1.5, 3, 120, '0', 1),
-(4, 'Centrale solaire', 'solar_plant', 'Récupère de l''énergie grâce au soleil, source inépuisable.', 'Pour assurer l''alimentation des mines et des synthétiseurs en énergie, des centrales électriques solaires géantes sont nécessaires. Plus ces installations sont développées, plus la surface de la planète est recouverte de cellules photovoltaïques qui transforment les rayons de soleil en énergie électrique. Les centrales électriques solaires sont la base de l''alimentation énergétique d''une planète.', 50, 40, NULL, NULL, 1.2, 3, 120, '0', 1),
-(5, 'Usine de robots', 'factory_robots', 'Les usines de robots produisent des robots ouvriers qui servent à la construction de l''infrastructure planétaire. Chaque niveau augmente la vitesse de construction des différents bâtiments.', 'Le développement de ce bâtiment permet d''améliorer la vitesse de construction des bâtiments, vaisseaux, défenses et recherches.', 600, 230, 300, NULL, 1.3, 4, 210, '', 1);
+INSERT INTO `building` (`id`, `name`, `name_clean`, `introduction`, `content`, `metal`, `crystal`, `deuterium`, `energy`, `multiplier`, `square`, `construct_time`, `important`) VALUES
+(1, 'Mine de métal', 'metal_mine', 'Une mine permettant d''extraire les métaux.', 'Principale matière première pour la construction de bâtiments et de vaisseaux. Le métal est la matière première la moins chère, mais c''est la plus importante. La production du métal consomme moins d''énergie que la production des autres ressources. Plus les mines sont développées, plus elles sont profondes. Les réserves minérales sont situées en profondeur sur la majorité des planètes. Les mines les plus profondes permettent d''extraire plus de métal, ce qui augment leur rendement. Cependant, les mines les plus développées consomment aussi plus d''énergie.', 50, 50, NULL, 8, 1.4, 2, 120, 1),
+(2, 'Mine de cristal', 'crystal_mine', 'Une mine permettant d''extraire les cristaux.', 'Le cristal est la principale ressource pour l''électronique et pour les alliages et son extraction consomme environ une fois et demi plus d''énergie que celle du métal, le cristal est donc plus précieux. Tous les vaisseaux et bâtiments ont besoin de cristal. Malheureusement, la plupart des cristaux nécessaires pour la construction de vaisseaux sont très rares et se trouvent en grande profondeur, comme le métal. La production augmente avec le développement des mines car on atteint des gisement plus grands et plus purs.', 50, 50, NULL, 8, 1.4, 2, 120, 1),
+(3, 'Synthétiseur de deuterium', 'deuterium_synthesizer', 'Extrait et synthétise le deutérium.', 'Le deutérium est produit à partir d''eau lourde. Les plus grandes quantités se trouvent donc en grande profondeur, comme le métal et le cristal. Le développement du bâtiment permet d''accéder à des réserves de deutérium en plus grande profondeur et de concentrer ce deutérium extrait de l''eau lourde. Le deutérium sert de carburant pour les vaisseaux et est nécessaire pour presque toutes les recherches.', 60, 30, NULL, 12, 1.5, 3, 120, 1),
+(4, 'Centrale solaire', 'solar_plant', 'Récupère de l''énergie grâce au soleil, source inépuisable.', 'Pour assurer l''alimentation des mines et des synthétiseurs en énergie, des centrales électriques solaires géantes sont nécessaires. Plus ces installations sont développées, plus la surface de la planète est recouverte de cellules photovoltaïques qui transforment les rayons de soleil en énergie électrique. Les centrales électriques solaires sont la base de l''alimentation énergétique d''une planète.', 50, 40, NULL, NULL, 1.2, 3, 120, 1),
+(5, 'Usine de robots', 'factory_robots', 'Les usines de robots produisent des robots ouvriers qui servent à la construction de l''infrastructure planétaire. Chaque niveau augmente la vitesse de construction des différents bâtiments.', 'Le développement de ce bâtiment permet d''améliorer la vitesse de construction des bâtiments, vaisseaux, défenses et recherches.', 600, 230, 300, NULL, 1.3, 4, 210, 1),
+(6, 'Chantier spatial', 'yardspace', 'Le chantier spatial permet de construire les vaisseaux et les installations de défense.', 'Le chantier spatial permet de construire les vaisseaux et les installations de défense. Plus le chantier est grand, plus la construction de vaisseaux et des installations de défense est rapide. La construction d''une usine de nanites permet la production de robots minuscules qui aident les robots ouvriers à travailler plus rapidement.', 850, 550, 300, NULL, 1.5, 5, 120, 1);
 
 -- --------------------------------------------------------
 
@@ -88,6 +88,26 @@ INSERT INTO `defenses` (`id`, `name`, `name_clean`, `introduction`, `content`, `
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `dependencies`
+--
+
+CREATE TABLE IF NOT EXISTS `dependencies` (
+  `element_id` int(11) NOT NULL,
+  `needed_element_id` int(11) NOT NULL,
+  `needed_level` tinyint(3) NOT NULL,
+  `type_element` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `dependencies`
+--
+
+INSERT INTO `dependencies` (`element_id`, `needed_element_id`, `needed_level`, `type_element`) VALUES
+(6, 5, 2, 'building');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `igame_sessions`
 --
 
@@ -105,8 +125,6 @@ CREATE TABLE IF NOT EXISTS `igame_sessions` (
 -- Contenu de la table `igame_sessions`
 --
 
-INSERT INTO `igame_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-('30bc0d1bc47a72f5bc437a00266b2ee5', '192.168.233.1', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7', 1324935395, 'a:6:{s:9:"user_data";s:0:"";s:8:"username";s:4:"test";s:5:"email";s:12:"test@mail.fr";s:2:"id";i:1;s:6:"logged";b:1;s:9:"planet_id";s:1:"1";}');
 
 -- --------------------------------------------------------
 
@@ -146,7 +164,7 @@ CREATE TABLE IF NOT EXISTS `planets` (
 --
 
 INSERT INTO `planets` (`id`, `user_id`, `name`, `galaxy`, `system`, `planet`, `created_at`, `updated_at`, `metal`, `crystal`, `deuterium`, `energy_used`, `energy_max`, `metal_mine`, `crystal_mine`, `deuterium_synthesizer`, `solar_plant`, `factory_robots`, `laboratory`, `yardspace`, `ion`, `laser`, `plasma`) VALUES
-(1, 1, 'planète de test', 1, 1, 1, '2011-12-01 00:00:00', '2011-12-26 22:36:37', 55103.22897796, 57867.59166667, 59262.86666669, 192, 74, 3, 3, 2, 2, 1, 1, 0, 0, 0, 0),
+(1, 1, 'planète de test', 1, 1, 1, '2011-12-01 00:00:00', '2012-02-04 18:28:03', 198411.39564463, 201175.75833334, 154848.31111115, 203, 74, 4, 3, 2, 2, 0, 0, 0, 0, 0, 0),
 (2, 1, 'youpi', 1, 1, 3, '2011-12-02 00:00:00', '2011-12-26 19:16:58', 64453.04166668, 65095.32222223, 61641.12222223, 28, 20, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
@@ -163,7 +181,7 @@ CREATE TABLE IF NOT EXISTS `queue` (
   `time_start` datetime NOT NULL,
   `time_finish` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Contenu de la table `queue`
