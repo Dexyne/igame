76d4d05e17ce4c7b4992060956a8aa78528da66a
Dexyne <enyriel@gmail.com>
2012-03-04 20:33:13 +0100

    Update css style for better ergonomics

--- application/views/game/building/index.php
+++ application/views/game/building/index.php
@@ -22,48 +22,50 @@
 
 <?php if(isset($notif)) : echo notif($notif['type'], $notif['message'], $notif['block'], $notif['heading']); endif; ?>
 
-<table>
-	<tr>
-		<th>&nbsp;</th>
-		<th>Informations</th>
-		<th>Level</th>
-		<th>Actions</th>
-	</tr>
-<?php foreach($list as $building) : ?>
-	<tr>
-		<td><?php echo img("building/{$building->name_clean}.gif", "Illustration : {$building->name}", $building->name) ?></td>
-		<td>
-			<h4>
-				&lsaquo;<?php echo anchor('game/building/show/'.$building->id, $building->name, 'class="anchor"') ?>&rsaquo;
-			<?php if($building->important > 0)
-				echo '<span class="label label-important">Important</span>'; ?>
-			</h4>
-			<p><?php echo $building->introduction ?></p>
-			<dl>
-				<dt>Ressource(s) nécessaire(s) :<dt>
-				<dd>
-					<?php if(!is_null($building->metal)) echo img('icons/metal.gif', 'métal', 'Métal')
-					.(floor($building->metal * ($building->level + 1) * $building->multiplier)) ?>
-					<?php if(!is_null($building->crystal)) echo img('icons/crystal.gif', 'cristal', 'Cristal')
-					.(floor($building->crystal * ($building->level + 1) * $building->multiplier)) ?>
-					<?php if(!is_null($building->deuterium)) echo img('icons/deuterium.gif', 'deutérium', 'Deutérium')
-					.(floor($building->deuterium * ($building->level + 1) * $building->multiplier)) ?>
-					<?php if(!is_null($building->energy)) echo img('icons/energy.gif', 'énergie', 'Energie')
-					.(floor($building->energy * ($building->level + 1) * $building->multiplier)) ?>
-				</dd>
-				<dd>Temps de construction :
-				<?php if($building->construct_time > 0) :
-					echo (($building->construct_time * ($building->level + 1) * $building->multiplier) / 60) ?> minutes
-				<?php else :
-					echo '<span class="label label-notice">Instantané</span>';
-				endif; ?>
-				</dd>
-			</dl>
-		</td>
-		<td class="center">
-			<?php echo $building->level ?>
-		</td>
-		<td><?php echo anchor("game/building/create/{$building->id}", "Construire ?"); ?></td>
-	</tr>
-<?php endforeach; ?>
+<table class="table table-striped table-condensed">
+	<thead>
+		<tr>
+			<th>&nbsp;</th>
+			<th><div class="center">Informations</div></th>
+			<th><div class="center">Level</div></th>
+			<th class="span2"><div class="center">Actions</div></th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php foreach($list as $building) : ?>
+		<tr>
+			<td class="span2"><?php echo img("building/{$building->name_clean}.gif", "Illustration : {$building->name}", $building->name) ?></td>
+			<td>
+				<h4>
+					&lsaquo;<?php echo anchor('game/building/show/'.$building->id, $building->name, 'class="anchor"') ?>&rsaquo;
+				<?php if($building->important > 0)
+					echo '<span class="label label-important">Important</span>'; ?>
+				</h4>
+				<p><?php echo $building->introduction ?></p>
+				<dl>
+					<dt>Ressource(s) nécessaire(s) :</dt>
+					<dd>
+						<?php if(!is_null($building->metal)) echo img('icons/metal.gif', 'métal', 'Métal')
+						.(floor($building->metal * ($building->level + 1) * $building->multiplier)) ?>
+						<?php if(!is_null($building->crystal)) echo img('icons/crystal.gif', 'cristal', 'Cristal')
+						.(floor($building->crystal * ($building->level + 1) * $building->multiplier)) ?>
+						<?php if(!is_null($building->deuterium)) echo img('icons/deuterium.gif', 'deutérium', 'Deutérium')
+						.(floor($building->deuterium * ($building->level + 1) * $building->multiplier)) ?>
+						<?php if(!is_null($building->energy)) echo img('icons/energy.gif', 'énergie', 'Energie')
+						.(floor($building->energy * ($building->level + 1) * $building->multiplier)) ?>
+					</dd>
+					<dd>Temps de construction :
+					<?php if($building->construct_time > 0) :
+						echo (($building->construct_time * ($building->level + 1) * $building->multiplier) / 60) ?> minutes
+					<?php else :
+						echo '<span class="label label-notice">Instantané</span>';
+					endif; ?>
+					</dd>
+				</dl>
+			</td>
+			<td><div class="center"><?php echo $building->level ?></div></td>
+			<td><div class="center"><?php echo anchor("game/building/create/{$building->id}", "Construire ?"); ?></div></td>
+		</tr>
+	<?php endforeach; ?>
+	</tbody>
 </table>
--- application/views/game/yardspace/index.php
+++ application/views/game/yardspace/index.php
@@ -28,40 +28,50 @@
 
 	<?php if(isset($notif)) : echo notif($notif['type'], $notif['message'], $notif['block'], $notif['heading']); endif; ?>
 
-	<table>
-		<tr>
-			<th>&nbsp;</th>
-			<th>Informations</th>
-			<th>Level</th>
-			<th>Actions</th>
-		</tr>
-	<?php foreach($list as $ship) : ?>
-		<tr>
-			<td><?php echo img("ship/{$ship->name_clean}.gif", "Illustration : {$ship->name}", $ship->name) ?></td>
-			<td>
-				<h4><?php echo $ship->name ?></h4>
-				<p><?php echo $ship->content ?></p>
-				<dl>
-					<dt>Ressource(s) nécessaire(s) :<dt>
-					<dd>
-						<?php if(!is_null($ship->metal)) echo img('icons/metal.gif', 'métal', 'Métal')
-						.(floor($ship->metal * ($ship->level + 1) * $ship->multiplier)) ?>
-						<?php if(!is_null($ship->crystal)) echo img('icons/crystal.gif', 'cristal', 'Cristal')
-						.(floor($ship->crystal * ($ship->level + 1) * $ship->multiplier)) ?>
-						<?php if(!is_null($ship->deuterium)) echo img('icons/deuterium.gif', 'deutérium', 'Deutérium')
-						.(floor($ship->deuterium * ($ship->level + 1) * $ship->multiplier)) ?>
-						<?php if(!is_null($ship->energy)) echo img('icons/energy.gif', 'énergie', 'Energie')
-						.(floor($ship->energy * ($ship->level + 1) * $ship->multiplier)) ?>
-					</dd>
-					<dd>Temps de construction : <?php echo (($ship->construct_time * ($ship->level + 1) * $ship->multiplier) / 60) ?> minutes</dd>
-				</dl>
-			</td>
-			<td class="center">
-				<?php echo $ship->level ?>
-			</td>
-			<td><?php echo anchor("game/ship/create/{$ship->id}", "Construire ?"); ?></td>
-		</tr>
-	<?php endforeach; ?>
+	<table class="table table-striped table-condensed">
+		<thead>
+			<tr>
+				<th>&nbsp;</th>
+				<th><div class="center">Informations</div></th>
+				<th><div class="center">Nombre</div></th>
+				<th class="span2"><div class="center">Actions</div></th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php foreach($list as $ship) : ?>
+			<tr>
+				<td class="span2"><?php echo img("ship/{$ship->name_clean}.gif", "Illustration : {$ship->name}", $ship->name) ?></td>
+				<td>
+					<h4>
+						&lsaquo;<?php echo anchor('game/ship/show/'.$ship->id, $ship->name, 'class="anchor"') ?>&rsaquo;
+					</h4>
+					<p><?php echo $ship->introduction ?></p>
+					<dl>
+						<dt>Ressource(s) nécessaire(s) :</dt>
+						<dd>
+							<?php if(!is_null($ship->metal)) echo img('icons/metal.gif', 'métal', 'Métal')
+							.(floor($ship->metal * ($ship->level + 1) * $ship->multiplier)) ?>
+							<?php if(!is_null($ship->crystal)) echo img('icons/crystal.gif', 'cristal', 'Cristal')
+							.(floor($ship->crystal * ($ship->level + 1) * $ship->multiplier)) ?>
+							<?php if(!is_null($ship->deuterium)) echo img('icons/deuterium.gif', 'deutérium', 'Deutérium')
+							.(floor($ship->deuterium * ($ship->level + 1) * $ship->multiplier)) ?>
+							<?php if(!is_null($ship->energy)) echo img('icons/energy.gif', 'énergie', 'Energie')
+							.(floor($ship->energy * ($ship->level + 1) * $ship->multiplier)) ?>
+						</dd>
+						<dd>Temps de construction :
+						<?php if($ship->construct_time > 0) :
+							echo (($ship->construct_time * ($ship->level + 1) * $ship->multiplier) / 60) ?> minutes
+						<?php else :
+							echo '<span class="label label-notice">Instantané</span>';
+						endif; ?>
+						</dd>
+					</dl>
+				</td>
+				<td><div class="center"><?php echo $ship->level ?></div></td>
+				<td><div class="center"><?php echo anchor("game/ship/create/{$ship->id}", "Construire ?"); ?></div></td>
+			</tr>
+		<?php endforeach; ?>
+		</tbody>
 	</table>
 
 <?php endif; ?>
